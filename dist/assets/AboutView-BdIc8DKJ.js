ec4ea55c94142a0f5c070710612dd6652cb7fd3e
jmanueltorress <tjmanuel354@gmail.com>
2026-03-22 16:05:46 -0600

    Build: agregar carpeta dist

---
+++ dist/assets/AboutView-BdIc8DKJ.js
@@ -0,0 +1,1 @@
+import{c as e,o as t}from"./index-OIxiaP5y.js";const n={__name:"AboutView",setup(o){return(a,c)=>(t(),e("main"))}};export{n as default};
---
+++ dist/assets/ServicesView-Cdga_9QO.js
@@ -0,0 +1,1 @@
+import{c as e,a,H as r,o as c}from"./index-OIxiaP5y.js";const _={__name:"ServicesView",setup(t){return(o,s)=>(c(),e("main",null,[a(r)]))}};export{_ as default};
